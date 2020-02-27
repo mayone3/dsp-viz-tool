d40979772bff765944e2448e4b80646ec9e8143b
Siwei Xian <sxian3@illinois.edu>
2020-02-27 17:16:51 -0600

    123

---
+++ web-app/js/AMPlayer.js
@@ -0,0 +1,21 @@
+import React
+import ReactDOM
+
+class AMPlayer extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>W O R D S</h1>
+            </div>
+        )
+    }
+}
+
+export default AMPlayer
--- web-app/js/test.js
+++ web-app/js/test.js
@@ -1,20 +1,46 @@
-var pi = 3.1415926535897932384626
-var samplerate = 44100.0
-var duration = 1.0
-var a = 1.0
-var f = 440.0
-var n = nj.arange(samplerate * duration, 'float64')
-var t = n.divide(samplerate)
+// import React
+// import ReactDOM
 
-// sample = A * sin(2 * pi * f * t)
-var sample = nj.sin(t.multiply(2*pi*f))
+import AMPlayer from "./AMPlayer.js"
 
-console.log(samplerate.toString())
-console.log(duration.toString())
-console.log(a.toString())
-console.log(f.toString())
-console.log(n.toString())
-console.log(t.toString())
-console.log(sample.toString())
+ReactDOM.render(<AMPlayer />, document.getElementById("root"))
 
-document.getElementById("mySample").innerHTML = sample.toString()
+// var pi = 3.1415926535897932384626
+// var samplerate = 44100.0
+// var duration = 1.0
+// var a = 1.0
+// var f = 440.0
+// var numPoints = 250
+// var n = nj.arange(samplerate * duration, 'float64')
+// var t = n.divide(samplerate)
+//
+// // sample = A * sin(2 * pi * f * t)
+// var sample = nj.sin(t.multiply(2*pi*f))
+//
+// console.log(samplerate.toString())
+// console.log(duration.toString())
+// console.log(a.toString())
+// console.log(f.toString())
+// console.log(n.toString())
+// console.log(t.toString())
+// console.log(sample.toString())
+//
+// var sampleFrequencyDomainPlot = document.getElementById('sample-frequency-domain-plot');
+// var sampleFrequencyDomainData = [{
+//     x: t.tolist().slice(0,numPoints),
+//     y: sample.tolist().slice(0,numPoints)
+// }]
+// var sampleFrequencyDomainLayout = {
+//     yaxis: {
+//         range: [-1.1, 1.1]
+//     },
+//     margin: {
+//         t: 0
+//     }
+// }
+//
+// Plotly.newPlot(
+//     sampleFrequencyDomainPlot,
+//     sampleFrequencyDomainData,
+//     sampleFrequencyDomainLayout
+// );
